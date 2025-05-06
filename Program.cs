38a927960aa33319d32c63155040f593be483e3c
Ari <churaa1@student.op.ac.nz>
2025-05-06 14:46:23 +1200

    build(c#): Project added ++

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SquishyRobotGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SQUISHY");
+            Console.ReadLine();
+        }
+    }
+}
