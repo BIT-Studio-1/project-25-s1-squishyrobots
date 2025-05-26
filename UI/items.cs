aebe700bcb49e8e58c6d34b9baebcdb4c1deae39
Ryan Hardy (1000136803) <HARDRJ3@student.op.ac.nz>
2025-05-26 14:39:52 +1200

    Added all Bools for grab-able items in 'items.cs'

--- UI/items.cs
+++ UI/items.cs
@@ -6,9 +6,42 @@ using System.Threading.Tasks;
 
 
 
-    public static class Items
+public static class Items
 {
+    
+    
     public static bool hasRedKey = false;
+
+
+    // Brig Items
+    public static bool hasHandcuffs = false;
+
+    // Captain Quarters Items
+    public static bool hasSword = false;
+
+    // Engine Room Items
+    public static bool hasWrench = false;
+    public static bool hasFuelCanister = false;
+    public static bool hasMaintenanceKit = false;
+
+    // Escape Pods Items
+    public static bool hasOxygenTank = false;
+    public static bool hasParachute = false;
+    public static bool hasLaunchKey = false;
+
+    // Hallway Items
+    public static bool hasWallMap = false;
+
+    // Med Bay Items
+    public static bool hasFirstAidKit = false;
+    public static bool hasSurgicalTool = false;
+    public static bool hasMedicineBottle = false;
+
+    //Shuttle Bay Items
+    public static bool hasShuttleFuel = false;
+
+
+
 
 
 
