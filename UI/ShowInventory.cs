ca45d9757962187aafec234b909d1a5bbc71c36c
Ryan Hardy (1000136803) <HARDRJ3@student.op.ac.nz>
2025-05-27 13:49:43 +1200

    Added Inventory 'if' statements

--- UI/ShowInventory.cs
+++ UI/ShowInventory.cs
@@ -4,9 +4,52 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SquishyRobotGame.UI
+internal class Inventory
 {
-    internal class ShowInventory
+    public static string ShowInventory(Inventory inventory)
     {
+        if (Items.hasRedKey == true)
+        {
+            Console.WriteLine("Red Key");
+        }
+
+
+        // Brig Items
+        if (Items.hasHandcuffs == true)
+        {
+            Console.WriteLine("Handcuffs");
+        }
+
+        // Captain Quarters Items
+        if (Items.hasRedKey == true)
+        {
+            Console.WriteLine("Red Key");
+        }
+
+        // Engine Room Items
+        if (Items.hasWrench == true)
+        {
+            Console.WriteLine("Wrench");
+        }
+        if (Items.hasFuelCanister == true)
+        {
+            Console.WriteLine("Fuel Canister");
+        }
+        if (Items.hasMaintenanceKit == true)
+        {
+            Console.WriteLine("Maintenance Kit");
+        }
+
+        //Escape Pods
+        if (Items.hasOxygenTank == true)
+        {
+            Console.WriteLine("Oxygen Tank");
+        }
+
+
+
+
+
+
     }
 }
