4865138de21217bf0e05449349d5b3f90153bdd1
BadCoderName1 <hardrj3@student.op.ac.nz>
2025-05-28 17:02:36 +1200

    Fixed method not returning value, replace writeline with formate.printspecial and fixed switch statement for player input

--- UI/ShowInventory.cs
+++ UI/ShowInventory.cs
@@ -7,140 +7,163 @@ using Globals;
 
 internal class Inventory
 {
-    public static string ShowInventory(Inventory inventory)
+    public static void ShowInventory()
     {
 
         do
         {
 
-            Console.WriteLine("-------------------------------------------------");
-            Console.WriteLine("You have the following items in your inventory: ");
-            Console.WriteLine("-------------------------------------------------");
+            Format.PrintSpecial("-------------------------------------------------");
+            Format.PrintSpecial("You have the following items in your inventory: ");
+            Format.PrintSpecial("-------------------------------------------------");
 
 
 
             // Starting Items
             if (Items.hasRedKey == true)
             {
-                Console.WriteLine("Red Key");
+                Format.PrintSpecial("Red Key");
             }
 
             // Brig Items
             if (Items.hasHandcuffs == true)
             {
-                Console.WriteLine("Handcuffs");
+                Format.PrintSpecial("Handcuffs");
             }
 
             // Captain Quarters Items
             if (Items.hasRedKey == true)
             {
-                Console.WriteLine("Red Key");
+                Format.PrintSpecial("Red Key");
             }
 
             // Engine Room Items
             if (Items.hasWrench == true)
             {
-                Console.WriteLine("Wrench");
+                Format.PrintSpecial("Wrench");
             }
             if (Items.hasFuelCanister == true)
             {
-                Console.WriteLine("Fuel Canister");
+                Format.PrintSpecial("Fuel Canister");
             }
             if (Items.hasMaintenanceKit == true)
             {
-                Console.WriteLine("Maintenance Kit");
+                Format.PrintSpecial("Maintenance Kit");
             }
 
             //Escape Pods
             if (Items.hasOxygenTank == true)
             {
-                Console.WriteLine("Oxygen Tank");
+                Format.PrintSpecial("Oxygen Tank");
             }
             if (Items.hasParachute == true)
             {
-                Console.WriteLine("Parachute");
+                Format.PrintSpecial("Parachute");
             }
             if (Items.hasLaunchKey == true)
             {
-                Console.WriteLine("Launch Key");
+                Format.PrintSpecial("Launch Key");
             }
 
             // Hallway Items
             if (Items.hasWallMap == true)
             {
-                Console.WriteLine("Wall Map");
+                Format.PrintSpecial("Wall Map");
             }
 
             // Med Bay Items
             if (Items.hasFirstAidKit == true)
             {
-                Console.WriteLine("First Aid Kit");
+                Format.PrintSpecial("First Aid Kit");
             }
             if (Items.hasSurgicalTool == true)
             {
-                Console.WriteLine("Surgical Tool");
+                Format.PrintSpecial("Surgical Tool");
             }
             if (Items.hasMedicineBottle == true)
             {
-                Console.WriteLine("Medicine Bottle");
+                Format.PrintSpecial("Medicine Bottle");
             }
             if (Items.hasShuttleFuel == true)
             {
-                Console.WriteLine("Shuttle Fuel");
+                Format.PrintSpecial("Shuttle Fuel");
             }
 
-            // if no items are in the inventory
+            // If no items are in the inventory
             if (!Items.hasRedKey && !Items.hasGreenKey && !Items.hasCrowbar && !Items.hasFuelCell && !Items.hasPurpleKey && !Items.hasBlueKey && !Items.hasHandcuffs
                 && !Items.hasSword && !Items.hasWrench && !Items.hasFuelCanister && !Items.hasMaintenanceKit && !Items.hasOxygenTank && !Items.hasLaunchKey && !Items.hasWallMap
                 && !Items.hasFirstAidKit && !Items.hasSurgicalTool && !Items.hasMaintenanceKit && !Items.hasShuttleFuel)
             {
-                Console.WriteLine("You have no items in your inventory.");
+                Format.PrintSpecial("You have no items in your inventory.");
             }
 
-            // instructions for exit
-            Console.WriteLine("\n-- type 'back' to return --\n");
+            // Instructions for exit
+            Format.PrintSpecial("\n-- type 'back' to return --\n");
 
-            
+
+            // Wait for input
+            Player.input = Console.ReadLine();
+
+
             // Player input for each item & back
             if (Player.input != "back")
             {
                 switch (Player.input)
                 {
+
+                    case "back":
+                        break;
                     case "red key":
+                        Console.Clear();
                         Format.PrintSpecial("A silver key card with two ^red stripes^ across the top.");
+                        Thread.Sleep(5000);
                         break;
                     case "green key":
+                        Console.Clear();
                         Format.PrintSpecial("You have a green key.");
+                        Thread.Sleep(5000);
                         break;
                     case "crowbar":
+                        Console.Clear();
                         Format.PrintSpecial("You have a crowbar.");
+                        Thread.Sleep(5000);
                         break;
                     case "fuel cell":
+                        Console.Clear();
                         Format.PrintSpecial("A beveled cylinder with a pulsing *blue bar* across its length. You get the feeling it still has an abundance of power stored within.");
+                        Thread.Sleep(5000);
                         break;
                     case "blue key":
+                        Console.Clear();
                         Format.PrintSpecial("You have a blue key");
+                        Thread.Sleep(5000);
                         break;
                     case "purple key":
+                        Console.Clear();
                         Format.PrintSpecial("You have a purple key");
+                        Thread.Sleep(5000);
                         break;
                     default:
+                        Console.Clear();
                         Format.PrintSpecial("^Unknown Item^");
+                        Thread.Sleep(5000);
                         break;
 
                 }
+            }
 
-                Format.PrintSpecial("\nPress %'enter'% to return");
-                Console.ReadLine();
-
-
-            } while (Player.input != "back");
+            Console.Clear();
 
 
-
-
-
-
-
-        }
+        } while (Player.input != "back");
+    }
 }
+
+
+
+
+
+
+
+
+ 
